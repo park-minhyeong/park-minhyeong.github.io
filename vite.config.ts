450b1ec0afd2833a10e526b501a01249394c45ad
minhyeong <minhyeong.dev@gmail.com>
2024-09-06 23:11:49 +0900

    [Chore] set workflows

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/park-minhyeong.github.io/",
   resolve: {
     alias: [
       { find: "@components", replacement: "/src/components" },
