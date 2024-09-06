2458ff561faf733930b2331118ba909f23e0fb0e
minhyeong <minhyeong.dev@gmail.com>
2024-09-06 23:44:33 +0900

    [Fix]

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/park-minhyeong.github.io/",
+  base: "/",
   resolve: {
     alias: [
       { find: "@components", replacement: "/src/components" },
