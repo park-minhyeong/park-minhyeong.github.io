51cb3d39dfeb023d2275975d14670f702ad27797
minhyeong <minhyeong.dev@gmail.com>
2024-09-09 02:08:49 +0900

    [Feat] inherit properties ```State```

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Terminal from "@/components/Terminal";
+import Action from "@/layout/Action";
+import { useState } from "react";
+
 export default function RootPage() {
-  return <div>minhyeong's portfolio</div>;
+  const commandState = useState<string>("");
+  return (
+    <Action.Replace actions={[[true, <Terminal state={commandState} />]]}>
+      <div>minhyeong's portfolio</div>
+    </Action.Replace>
+  );
 }
--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,6 +1,12 @@
 import { cn } from "@/util";
+import { State } from "@/interface";
 
-export default function Terminal() {
+interface TerminalProps {
+  state: State<string>;
+}
+
+export default function Terminal({ state }: TerminalProps) {
+  const [command, setCommand] = state;
   const container = {
     sizes: "w-full min-w-[800px] h-[600px]",
     backgrounds: "bg-gray-500",
